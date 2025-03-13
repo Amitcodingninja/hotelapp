97080ed819e0257382e97412ef6d2e19f3311244
Amit Kumar <akumar420india@gmail.com>
2025-03-13 09:20:55 +0530

    Initial commit

---
+++ src/main/java/com/kafka/HotelappApplication.java
@@ -0,0 +1,23 @@
+package com.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import com.kafka.constants.AppConstants;
+
+@SpringBootApplication
+public class HotelappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelappApplication.class, args);
+    }
+
+    @KafkaListener(topics = AppConstants.TOPIC, groupId = "group_customer_order")
+    public void kafakSubscriberContent(String order) {
+        System.out.print("_____________ Msg fecthed From Kafka_________________");
+        System.out.println(order);
+
+    }
+
+}
